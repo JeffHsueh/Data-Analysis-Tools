ee47cbc995c73240a1c5e00861d609298274ec10
Jeff Hsueh <JeffHsueh@users.noreply.github.com>
2015-11-01 22:50:00 +0800

    Update week2_Chi_square.py

--- week2_Chi_square.py
+++ week2_Chi_square.py
@@ -24,7 +24,11 @@ df3.rename(columns= {'2007':'maleemployrate'}, inplace=True) #rename column
 
 ## Group by mean
 ipp_mean = df3['incomeperperson'].dropna().mean()
-
+first = df3['incomeperperson'].quantile(q=0.2)
+second = df3['incomeperperson'].quantile(q=0.4)
+third  = df3['incomeperperson'].quantile(q=0.6)
+fourth = df3['incomeperperson'].quantile(q=0.8)
+fifth = df3['incomeperperson'].quantile(q=1)
 def meangroup (row):
     if row['incomeperperson'] >=ipp_mean:
         return 1
@@ -34,51 +38,70 @@ def meangroup (row):
 df3['meangroup'] = df3.apply(lambda row : meangroup(row),axis=1)
 c3= df3.groupby('meangroup').size()
 
-#cut into 4 groups
-df3['income4groups'] = pd.qcut(df3.incomeperperson, 4, labels=["Terrible","Bad","Decent","Great"]).dropna()
-df3['income4groups'] = df3['income4groups'].astype('category')
-df3['polityscore'] = df3['polityscore'].astype('category')
-#ct1 =  df3.groupby('income4groups').mean()
-#print (ct1)
-
+#cut into 5 groups
+def income5groups(row):
+    if row['incomeperperson'] <= first:
+        return "1_Terrible"
+    elif row['incomeperperson'] <=second:
+        return "2_Bad"
+    elif row['incomeperperson'] <= third:
+        return "3_Average"
+    elif row['incomeperperson'] <= fourth:
+        return "4_Decent"
+    elif row['incomeperperson'] <= fifth:
+        return "5_Great"
+        
+df3['income5groups'] = df3.apply(lambda row : income5groups(row),axis=1)
+df3['income5groups'] = df3['income5groups'].astype('category')
 # contingency table of observed counts
-ct2=pd.crosstab(df3['polityscore'] , df3['income4groups'])
-print (ct2)
+ct1=pd.crosstab(df3['polityscore'] , df3['income5groups'])
+print (ct1)
 
 # column percentages
-colsum=ct2.sum(axis=0)
-colpct=ct2/colsum
+colsum=ct1.sum(axis=0)
+colpct=ct1/colsum
+print (colsum)
 print(colpct)
 
 # chi-square
 print ('chi-square value, p value, expected counts')
-cs2= scipy.stats.chi2_contingency(ct2)
-print (cs2)
+cs1= scipy.stats.chi2_contingency(ct1)
+print (ct1)
 
-# new code for setting variables to numeric:
+#code for setting variables to numeric:
 df3['polityscore'] = df3['polityscore'].convert_objects(convert_numeric=True)
 
 # graph percent with nicotine dependence within each smoking frequency group 
-seaborn.factorplot(x="income4groups", y="polityscore", data=df3, kind="bar", ci=None)
-plt.xlabel('income4groups')
+seaborn.factorplot(x="income5groups", y="polityscore", data=df3, kind="bar", ci=None)
+plt.xlabel('income5groups')
 plt.ylabel('polityscore')
 plt.show()
 
 
 
 # Terrible vs. Bad
-recode1 ={'Terrible': 'Terrible', 'Bad':'Bad'}
-df3['terriblevsbad'] = df3['income4groups'].map(recode1)
-ct3 = pd.crosstab(df3['polityscore'], df3['terriblevsbad'])
+recode ={'1_Terrible': 'Terrible', '2_Bad':'Bad'}
+df3['terriblevsbad'] = df3['income5groups'].map(recode)
+ct2 = pd.crosstab(df3['polityscore'], df3['terriblevsbad'])
+colsum= ct2.sum(axis=0)
+colpct = ct2/colsum
+print ('Terrible vs. Bad\nchi-square value, p value, expected counts')
+cs2= scipy.stats.chi2_contingency(ct2)
+print (cs2)
+
+# Terrible vs Average
+recode1 ={'1_Terrible': 'Terrible', '3_Average':'Average'}
+df3['terriblevsaverage'] = df3['income5groups'].map(recode1)
+ct3 = pd.crosstab(df3['polityscore'], df3['terriblevsaverage'])
 colsum= ct3.sum(axis=0)
 colpct = ct3/colsum
-print ('Terrible vs. Bad\nchi-square value, p value, expected counts')
+print ('Terrible vs Average\nchi-square value, p value, expected counts')
 cs3= scipy.stats.chi2_contingency(ct3)
 print (cs3)
 
 # Terrible vs Decent
-recode2 ={'Terrible': 'Terrible', 'Decent':'Decent'}
-df3['terriblevsdecent'] = df3['income4groups'].map(recode2)
+recode2 ={'1_Terrible': 'Terrible', '4_Decent':'Decent'}
+df3['terriblevsdecent'] = df3['income5groups'].map(recode2)
 ct4 = pd.crosstab(df3['polityscore'], df3['terriblevsdecent'])
 colsum= ct4.sum(axis=0)
 colpct = ct4/colsum
@@ -87,8 +110,8 @@ cs4= scipy.stats.chi2_contingency(ct4)
 print (cs4)
 
 #Terrible vs. Great
-recode3 ={'Terrible': 'Terrible', 'Great':'Great'}
-df3['terriblevsgreat'] = df3['income4groups'].map(recode3)
+recode3 ={'1_Terrible': 'Terrible', '5_Great':'Great'}
+df3['terriblevsgreat'] = df3['income5groups'].map(recode3)
 ct5 = pd.crosstab(df3['polityscore'], df3['terriblevsgreat'])
 colsum= ct5.sum(axis=0)
 colpct = ct5/colsum
@@ -96,9 +119,20 @@ print ('Terrible vs. Great\nchi-square value, p value, expected counts')
 cs5= scipy.stats.chi2_contingency(ct5)
 print (cs5)
 
+# Bad vs Average
+recode3plus ={'2_Bad': 'Bad', '3_Average':'Average'}
+df3['badvsaverage'] = df3['income5groups'].map(recode3plus)
+ct5plus = pd.crosstab(df3['polityscore'], df3['terriblevsgreat'])
+colsum= ct5plus.sum(axis=0)
+colpct = ct5plus/colsum
+print ('Bad vs Average\nchi-square value, p value, expected counts')
+cs5plus= scipy.stats.chi2_contingency(ct5plus)
+print (cs5plus)
+
+
 #Bad vs. Decent
-recode4 ={'Bad': 'Bad', 'Decent':'Decent'}
-df3['badvsdecent'] = df3['income4groups'].map(recode4)
+recode4 ={'2_Bad': 'Bad', '4_Decent':'Decent'}
+df3['badvsdecent'] = df3['income5groups'].map(recode4)
 ct6 = pd.crosstab(df3['polityscore'], df3['badvsdecent'])
 colsum= ct6.sum(axis=0)
 colpct = ct6/colsum
@@ -107,8 +141,8 @@ cs6= scipy.stats.chi2_contingency(ct6)
 print (cs6)
 
 # Bad vs Great
-recode5 ={'Bad': 'Bad', 'Great':'Great'}
-df3['badvsgreat'] = df3['income4groups'].map(recode5)
+recode5 ={'2_Bad': 'Bad', '5_Great':'Great'}
+df3['badvsgreat'] = df3['income5groups'].map(recode5)
 ct7 = pd.crosstab(df3['polityscore'], df3['badvsgreat'])
 colsum= ct7.sum(axis=0)
 colpct = ct7/colsum
@@ -116,9 +150,32 @@ print ('Bad vs Great\nchi-square value, p value, expected counts')
 cs7= scipy.stats.chi2_contingency(ct7)
 print (cs7)
 
+#Average vs. Decent
+recode5plus = {'3_Average':'Average', "4_Decent":"Decent"}
+df3['averagevsdecent'] = df3['income5groups'].map(recode5plus)
+ct7plus = pd.crosstab(df3['polityscore'], df3['averagevsdecent'])
+colsum= ct7plus.sum(axis=0)
+colpct = ct7plus/colsum
+print ('Average vs. Decent\nchi-square value, p value, expected counts')
+cs7plus= scipy.stats.chi2_contingency(ct7plus)
+print (cs7plus)
+
+
+# Average vs Great
+recode5plus2 = {'3_Average':'Average',  '5_Great':'Great'}
+df3['averagevsgreat'] = df3['income5groups'].map(recode5plus2)
+ct7plus2 = pd.crosstab(df3['polityscore'], df3['averagevsgreat'])
+colsum= ct7plus2.sum(axis=0)
+colpct = ct7plus2/colsum
+print ('Average vs Great\nchi-square value, p value, expected counts')
+cs7plus2= scipy.stats.chi2_contingency(ct7plus2)
+print (cs7plus2)
+
+
+
 # Decent vs Great
-recode6 ={'Decent':'Decent', 'Great':'Great'}
-df3['decentvsgreat'] = df3['income4groups'].map(recode6)
+recode6 ={'4_Decent':'Decent', '5_Great':'Great'}
+df3['decentvsgreat'] = df3['income5groups'].map(recode6)
 ct8 = pd.crosstab(df3['polityscore'], df3['decentvsgreat'])
 colsum= ct8.sum(axis=0)
 colpct = ct8/colsum
